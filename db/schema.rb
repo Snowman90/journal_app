831aaef63489a6877c6a2f96cdd93e9c15f538d3
Snowman90 <woj.krzywda@gmail.com>
2019-07-24 18:15:30 +0200

    Add Weather model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_183021) do
+ActiveRecord::Schema.define(version: 2019_07_24_161410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2019_07_23_183021) do
     t.string "email"
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "weathers", force: :cascade do |t|
+    t.integer "temperature"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
