9cf48d159666c607c2ea75ebe68dfb1b4f8792bc
Snowman90 <woj.krzywda@gmail.com>
2019-07-29 09:48:23 +0200

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,16 @@
 class User < ApplicationRecord
-  def self.find_or_create_from_auth_hash(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.first_name = auth.info.first_name
-      user.last_name = auth.info.last_name
-      user.email = auth.info.email
-      user.save!
-    end
+  has_many :posts
+
+  def self.update_or_create(auth)
+    user = User.find_by(uid: auth[:uid]) || User.new
+    user.attributes = {
+      provider: auth[:provider],
+      uid: auth[:uid],
+      email: auth[:info][:email],
+      first_name: auth[:info][:first_name],
+      last_name: auth[:info][:last_name],
+    }
+    user.save!
+    user
   end
 end
