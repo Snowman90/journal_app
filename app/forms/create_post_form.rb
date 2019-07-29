f4bca7521c13c846037e87dece7a96c3cf7a7289
Snowman90 <woj.krzywda@gmail.com>
2019-07-29 12:19:22 +0200

    Add create_post_form form

---
+++ app/forms/create_post_form.rb
@@ -0,0 +1,34 @@
+class CreatePostForm
+  include ActiveModel::Model
+
+  attr_accessor :note, :city_name, :user_id, :temperature
+
+  validates :note, presence: true
+  validates :city_name, presence: true
+  validates :user_id, presence: true
+  validates_presence_of :temperature, message: "can't be read, provide valid city"
+
+  def save
+    return false unless valid?
+
+    post.save
+  end
+
+  private
+
+  def post
+    @post = Post.new(
+      note: note,
+      city: city,
+      user_id: user_id,
+    )
+  end
+
+  def city
+    @city = City.find_or_create_by(name: city_name, weather: weather)
+  end
+
+  def weather
+    @weather = Weather.find_or_create_by(temperature: temperature)
+  end
+end
