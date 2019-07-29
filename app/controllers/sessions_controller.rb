334602242bf08f6cbacd9c73e17c64a26d74d2e8
Snowman90 <woj.krzywda@gmail.com>
2019-07-29 10:17:02 +0200

    Update sessions controller, add notices

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_from_auth_hash(request.env['omniauth.auth'])
+    @user = User.update_or_create(request.env['omniauth.auth'])
     session[:user_id] = @user.id
-    redirect_to :posts
+    redirect_to :posts, notice: "Welcome #{@user.first_name}!"
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_path
+    redirect_to root_path, notice: "Successful logout"
   end
 end
